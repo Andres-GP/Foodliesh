30cbdc45d361d785d74817a9a2501cb35c27ae5c
Andres-GP <andresgarciapedreros@gmail.com>
2021-11-27 16:34:20 +0000

    authScreen implemented and login and register dart docs implemented

--- lib/authentication/auth_screen.dart
+++ lib/authentication/auth_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:hello_world/authentication/login.dart';
+import 'package:hello_world/authentication/register.dart';
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({ Key? key }) : super(key: key);
@@ -10,10 +12,55 @@ class AuthScreen extends StatefulWidget {
 class _AuthScreenState extends State<AuthScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text("Wellcome"),
-      ),
-    );
+    return DefaultTabController(
+      length: 2,
+      child: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              //TODO implement Color
+              color: Colors.indigo
+              ),
+          ),
+          title: const Text(
+            "Foodliesh",
+            style: TextStyle(
+              fontSize: 60,
+              color: Colors.white,
+              //TODO implement font family
+              fontFamily: "",
+            ),
+          ),
+          centerTitle: true,
+          bottom: const TabBar(
+            tabs: [
+              Tab(
+                icon: Icon(Icons.lock, color: Colors.white,),
+                text:  "Login",
+              ),
+              Tab(
+                icon: Icon(Icons.person, color: Colors.white),
+                text:  "Register",
+              ),
+          ],
+          indicatorColor: Colors.white38,
+          indicatorWeight: 6,
+          ),
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+            //TODO implement color
+            color: Colors.red,
+            ),
+            child: const TabBarView(
+              children: [
+                LoginScreen(),
+                RegisterScreen(),
+              ],
+              ),
+          ),
+        )
+      );
   }
 }
